3f65bddb3544ed5d4c30c685e9370950e6758211
Serhii151 <122047001+Serhii151@users.noreply.github.com>
2024-11-11 15:28:24 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
+
+
 import ContactForm from './components/ContactForm/ContactForm';
 import ContactList from './components/ContactList/ContactList';
 import Filter from './components/Filter/Filter'
@@ -8,17 +7,15 @@ import { AppContainer } from './App.styles';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+    
         <AppContainer>
-          <h1>Книга контактів</h1>
+          <h1>Phonebook</h1>
           <ContactForm />
           <h2>Contacts</h2>
           <Filter />
           <ContactList />
         </AppContainer>
-      </PersistGate>
-    </Provider>
+      
   );
 };
 
