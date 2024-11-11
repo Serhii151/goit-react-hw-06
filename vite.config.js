593bff2b95909f834ac8c3aa2f28e2040f6f69bb
Serhii151 <122047001+Serhii151@users.noreply.github.com>
2024-11-11 13:29:22 +0200

    up

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Serhii151.github.io/goit-react-hw-06/',
+  base: '/goit-react-hw-06/',
   build: {
     sourcemap: true,
   }
