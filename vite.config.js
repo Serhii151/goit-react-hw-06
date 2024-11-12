d33a4398354050e416893a76768965b688f42c1f
Serhii151 <122047001+Serhii151@users.noreply.github.com>
2024-11-12 13:46:06 +0200

    up

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/goit-react-hw-06/', 
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 });
